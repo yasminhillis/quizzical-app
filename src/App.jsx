d35231e8b140ac6c70d67576a297c0eec3be2f3e
Yasmin Hillis <.com>
2025-07-23 15:19:47 +0300

    finished the intro page #1

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ function App() {
 
   return (
     <>
-      
+      <div className='shape top-right'></div>
+      <h1 className='main-heading'>Quizzical</h1>
+      <h2 className='description'>Test your knowledge with fun quizzes!</h2>
+      <button className='start-quiz'>Start quiz</button>
+      <div className='shape bottom-left'></div>
+
     </>
   )
 }
